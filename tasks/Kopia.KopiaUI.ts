8d997ecd535f3a5046a8427c73d68aff0c736263
UnownPlain <git@unownplain.dev>
2025-05-20 23:39:18 -0400

    Add Kopia

---
+++ tasks/Kopia.KopiaUI.ts
@@ -0,0 +1,16 @@
+import { electronBuilder } from '../src/helpers.ts';
+import { updatePackage } from '../src/komac.ts';
+
+export default async function () {
+	const response = await fetch(
+		'https://github.com/kopia/kopia/releases/latest/download/latest.yml'
+	);
+	const versionInfo = await response.text();
+
+	const version = electronBuilder(versionInfo);
+	const urls = [
+		`https://github.com/kopia/kopia/releases/download/v${version}/KopiaUI-Setup-${version}.exe`,
+	];
+
+	await updatePackage('Kopia.KopiaUI', version, urls);
+}
