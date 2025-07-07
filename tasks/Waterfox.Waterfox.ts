88fbfd6370e27166c6d2693e041bfcc01f4e964d
UnownPlain <git@unownplain.dev>
2025-07-07 15:27:19 -0400

    Fix Waterfox.Waterfox

--- tasks/Waterfox.Waterfox.ts
+++ tasks/Waterfox.Waterfox.ts
@@ -3,7 +3,7 @@ import { redirectMatch } from '../src/helpers.ts';
 export default async function () {
 	const { version, url } = await redirectMatch(
 		'https://cdn1.waterfox.net/waterfox/releases/latest/windows',
-		/Waterfox[._-]Setup[._-](\d+(?:\.\d+)+)\.exe/i,
+		/Waterfox\s+Setup\s+(\d+(?:\.\d+)+)\.exe/i,
 	);
 
 	return {
