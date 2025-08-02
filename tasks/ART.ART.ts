64a3bbac568727f9f830adde9d4a041461d7d603
UnownPlain <git@unownplain.dev>
2025-08-02 12:49:53 -0400

    Fix ART.ART

--- tasks/ART.ART.ts
+++ tasks/ART.ART.ts
@@ -1,7 +1,7 @@
-import { getLatestPreRelease } from '../src/github.ts';
+import { getLatestRelease } from '../src/github.ts';
 
 export default async function () {
-	const version = await getLatestPreRelease('artpixls', 'ART');
+	const version = await getLatestRelease('artpixls', 'ART');
 	const urls = [
 		`https://github.com/artpixls/ART/releases/download/${version}/ART_${version}_Win64.exe`,
 		`https://github.com/artpixls/ART/releases/download/${version}/ART_${version}_Windows_arm64.exe`,
