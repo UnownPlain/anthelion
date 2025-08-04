dede87a18e70f227cc00554b5b3286c823eb67a3
UnownPlain <git@unownplain.dev>
2025-08-04 19:40:08 -0400

    Fix CPUID.CPU-Z

--- tasks/CPUID.CPU-Z.ts
+++ tasks/CPUID.CPU-Z.ts
@@ -1,15 +1,14 @@
 import { pageMatch } from '../src/helpers.ts';
 
 export default async function () {
-	const version = (
+	const versionInfo = (
 		await pageMatch(
 			'https://download.cpuid.com/cpuid.ver',
 			/cpuz=(\d+(?:\.\d+)+)/i,
 		)
-	)
-		.split('.')
-		.slice(0, -1)
-		.join('.');
+	).split('.');
+
+	const version = `${versionInfo[0]}.${versionInfo[1]}${versionInfo[2]}`;
 	const urls = [`https://download.cpuid.com/cpu-z/cpu-z_${version}-en.exe`];
 
 	return {
