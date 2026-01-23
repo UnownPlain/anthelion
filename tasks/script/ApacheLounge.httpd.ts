e3a17096cda9906f4ce097a8f06ddcf1c94ed767
UnownPlain <git@unownplain.dev>
2026-01-23 16:27:28 -0500

    Fix ApacheLounge.httpd

--- tasks/script/ApacheLounge.httpd.ts
+++ tasks/script/ApacheLounge.httpd.ts
@@ -6,8 +6,8 @@ export default async function () {
 	const versionInfo = await ky('https://www.apachelounge.com/download/').text();
 	const [version, date] = match(versionInfo, /httpd-([\d.]+)-(\d+)-(?:win32|win64)-vs\d+\.zip/i);
 	const urls = [
-		`https://www.apachelounge.com/download/VS17/binaries/httpd-${version}-${date}-Win64-VS17.zip`,
-		`https://www.apachelounge.com/download/vs17/binaries/httpd-${version}-${date}-win32-vs17.zip`,
+		`https://www.apachelounge.com/download/VS18/binaries/httpd-${version}-${date}-Win64-VS18.zip`,
+		`https://www.apachelounge.com/download/vs18/binaries/httpd-${version}-${date}-win32-vs18.zip`,
 	];
 
 	return {
