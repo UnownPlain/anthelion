800b7a3f6c21ac1d9ec41344e98b6c669c16b344
UnownPlain <git@unownplain.dev>
2026-02-12 16:44:50 -0500

    Fix Google.AndroidStudio.*

---
+++ tasks/script/Google.AndroidStudio.ts
@@ -0,0 +1,17 @@
+import ky from 'ky';
+
+import { match } from '@/helpers.ts';
+
+export default async function () {
+	const response = await ky('https://developer.android.com/studio/').text();
+	const [url, version] = match(
+		response,
+		/studio_win_bundle_download[\s\S]+?href="(https:\/\/edgedl\.me\.gvt1\.com\/android\/studio\/install\/(\d+(?:\.\d+)+)\/android-studio-[^"]*windows\.exe)"/i,
+	);
+
+	return {
+		version,
+		urls: [`${url}|x64`],
+		args: ['--release-notes-url', 'https://androidstudio.googleblog.com/'],
+	};
+}
