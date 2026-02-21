5302d4a35f67afdaa2ac76d88344ba907d7b738f
UnownPlain <git@unownplain.dev>
2026-02-21 15:08:35 -0500

    Update Zen-Team.Zen-Browser.Twilight tag

--- tasks/script/Zen-Team.Zen-Browser.Twilight.ts
+++ tasks/script/Zen-Team.Zen-Browser.Twilight.ts
@@ -2,15 +2,15 @@ import { getReleaseByTag } from '@/github.ts';
 import { match } from '@/helpers.ts';
 
 export default async function () {
-	const release = await getReleaseByTag('zen-browser', 'desktop', 'twilight');
+	const release = await getReleaseByTag('zen-browser', 'desktop', 'twilight-1');
 	const state = release.name || '';
 	const [version] = match(state, /Twilight build - (\S+)/);
 
 	return {
 		version,
 		urls: [
-			'https://github.com/zen-browser/desktop/releases/download/twilight/zen.installer.exe|x64',
-			'https://github.com/zen-browser/desktop/releases/download/twilight/zen.installer-arm64.exe',
+			'https://github.com/zen-browser/desktop/releases/download/twilight-1/zen.installer.exe|x64',
+			'https://github.com/zen-browser/desktop/releases/download/twilight-1/zen.installer-arm64.exe',
 		],
 		replace: true,
 		skipPrCheck: true,
