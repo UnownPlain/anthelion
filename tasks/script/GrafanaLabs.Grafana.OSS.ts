8442844121c2885beab7a4e3b5b0b41374f8dbbc
UnownPlain <git@unownplain.dev>
2026-03-04 19:11:32 -0500

    Add GrafanaLabs.Grafana.OSS.*

---
+++ tasks/script/GrafanaLabs.Grafana.OSS.ts
@@ -0,0 +1,18 @@
+import ky from 'ky';
+
+import { match } from '@/helpers.ts';
+
+export default async function () {
+	const response = await ky(
+		'https://grafana.com/grafana/download?edition=oss&platform=windows',
+	).text();
+	const [url, version] = match(
+		response,
+		/(https:\/\/dl\.grafana\.com\/grafana\/release\/(\d+(?:\.\d+)+)\/grafana_[^"' >]+?_windows_amd64\.msi)/i,
+	);
+
+	return {
+		version,
+		urls: [url],
+	};
+}
