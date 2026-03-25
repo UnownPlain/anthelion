1b3fdbc9cb15b3583f8d1170059635809c928486
UnownPlain <git@unownplain.dev>
2026-03-24 20:45:13 -0400

    Add Microsoft.OfficeDeploymentTool

---
+++ tasks/script/Microsoft.OfficeDeploymentTool.ts
@@ -0,0 +1,19 @@
+import ky from 'ky';
+
+import { match } from '@/helpers.ts';
+
+export default async function () {
+	const response = await ky(
+		'https://www.microsoft.com/en-us/download/details.aspx?id=49117',
+	).text();
+
+	const [url, version] = match(
+		response,
+		/"url":"(https:\/\/download\.microsoft\.com\/download\/[^"' >]*officedeploymenttool[^"' >]*\.exe)","size":"[^"' >]+","version":"(\d+(?:\.\d+)+)"/i,
+	);
+
+	return {
+		version,
+		urls: [url],
+	};
+}
