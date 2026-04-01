0a52bd4a61b90634620cb62a3bf596efdd163f92
UnownPlain <git@unownplain.dev>
2026-04-01 18:23:32 -0400

    Add all packages option to pkgtest

--- src/test.ts
+++ src/test.ts
@@ -1,5 +1,5 @@
-import { vs } from './helpers';
-import { runAllTasks } from './main';
+import { vs } from '@/helpers';
+import { runAllTasks } from '@/main';
 
 const tasks = vs(process.argv[2]).split(' ');
 
@@ -7,7 +7,7 @@ if (process.argv[3] === '--dry-run') {
 	process.env.DRY_RUN = 'true';
 }
 
-const failureCount = await runAllTasks(tasks);
+const failureCount = await runAllTasks(tasks[0] === 'all' ? undefined : tasks);
 
 if (failureCount > 0) {
 	process.exit(1);
