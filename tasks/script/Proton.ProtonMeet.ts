436a4252b405e661f24310c06c545af2d361f3eb
UnownPlain <git@unownplain.dev>
2026-04-01 16:53:20 -0400

    Add Proton.ProtonMeet

---
+++ tasks/script/Proton.ProtonMeet.ts
@@ -0,0 +1,16 @@
+import ky from 'ky';
+
+export default async function () {
+	const releases = await ky('https://proton.me/download/meet/windows/version.json').json<{
+		Releases: Array<{ CategoryName: string; Version: string }>;
+	}>();
+	const versions = releases.Releases.filter((version) => version.CategoryName === 'Stable');
+
+	const version = versions[0]?.Version;
+	const urls = [`https://proton.me/download/meet/windows/${version}/ProtonMeet-desktop.exe`];
+
+	return {
+		version,
+		urls,
+	};
+}
