4473a43d9611b239f259108ca49bc1d15acfbe51
UnownPlain <git@unownplain.dev>
2026-04-24 11:33:31 -0400

    Add Intel.IntelDriverAndSupportAssistant

---
+++ tasks/script/Intel.IntelDriverAndSupportAssistant.ts
@@ -0,0 +1,16 @@
+import ky from 'ky';
+
+export default async function () {
+	const response = await ky.head('https://dsadata.intel.com/installer');
+	const state = response.headers.get('Last-Modified');
+
+	if (!state) {
+		throw new Error('No Last-Modified header found');
+	}
+
+	return {
+		version: 'displayVersion',
+		urls: ['https://dsadata.intel.com/installer'],
+		state,
+	};
+}
