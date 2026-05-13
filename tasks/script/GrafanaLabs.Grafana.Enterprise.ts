aa05fcb02b20f50475bf89a3b68d01c61bde76f1
UnownPlain <git@unownplain.dev>
2026-05-12 21:34:25 -0400

    Fix GrafanaLabs.Grafana.*

--- tasks/script/GrafanaLabs.Grafana.Enterprise.ts
+++ tasks/script/GrafanaLabs.Grafana.Enterprise.ts
@@ -6,10 +6,12 @@ export default async function () {
 	const response = await ky(
 		'https://grafana.com/grafana/download?edition=enterprise&platform=windows',
 	).text();
-	const [url, version] = match(
+	const [url, upstreamVersion] = match(
 		response,
-		/(https:\/\/dl\.grafana\.com\/grafana-enterprise\/release\/(\d+(?:\.\d+)+)\/grafana-enterprise_[^"' >]+?_windows_amd64\.msi)/i,
+		/(https:\/\/dl\.grafana\.com\/grafana-enterprise\/release\/(\d+(?:\.\d+)+(?:\+security-\d+)?)\/grafana-enterprise_[^"' >]+?_windows_amd64\.msi)/i,
 	);
+
+	const version = upstreamVersion?.replace(/\+security-(\d+)$/i, '.$1');
 
 	return {
 		version,
--- tasks/script/GrafanaLabs.Grafana.OSS.ts
+++ tasks/script/GrafanaLabs.Grafana.OSS.ts
@@ -6,10 +6,12 @@ export default async function () {
 	const response = await ky(
 		'https://grafana.com/grafana/download?edition=oss&platform=windows',
 	).text();
-	const [url, version] = match(
+	const [url, upstreamVersion] = match(
 		response,
-		/(https:\/\/dl\.grafana\.com\/grafana\/release\/(\d+(?:\.\d+)+)\/grafana_[^"' >]+?_windows_amd64\.msi)/i,
+		/(https:\/\/dl\.grafana\.com\/grafana\/release\/(\d+(?:\.\d+)+(?:\+security-\d+)?)\/grafana_[^"' >]+?_windows_amd64\.msi)/i,
 	);
+
+	const version = upstreamVersion?.replace(/\+security-(\d+)$/i, '.$1');
 
 	return {
 		version,
