8b5f62d4fcd666f11b0838e917aa97c88534c87e
UnownPlain <git@unownplain.dev>
2026-05-16 00:03:14 -0400

    Switch Intel.IntelDriverAndSupportAssistant to X-DSA-Hash header

--- tasks/script/Intel.IntelDriverAndSupportAssistant.ts
+++ tasks/script/Intel.IntelDriverAndSupportAssistant.ts
@@ -2,10 +2,10 @@ import ky from 'ky';
 
 export default async function () {
 	const response = await ky.head('https://dsadata.intel.com/installer');
-	const state = response.headers.get('Last-Modified');
+	const state = response.headers.get('X-DSA-Hash');
 
 	if (!state) {
-		throw new Error('No Last-Modified header found');
+		throw new Error('No X-DSA-Hash header found');
 	}
 
 	return {
