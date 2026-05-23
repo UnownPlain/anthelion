ea9654b5d057c5ad7b4a771c29f529b691f732fa
UnownPlain <git@unownplain.dev>
2026-05-23 19:52:30 -0400

    Add Google.AndroidCLI

---
+++ tasks/script/Google.AndroidCLI.ts
@@ -0,0 +1,19 @@
+import ky from 'ky';
+
+import { match } from '@/helpers.ts';
+
+export default async function () {
+	const response = await ky(
+		'https://dl.google.com/android/cli/latest/windows_x86_64/android.exe',
+	).arrayBuffer();
+	const binary = Buffer.from(response).toString('latin1');
+	const [version] = match(binary, /version=(\d+(?:\.\d+)+)\b/i);
+
+	return {
+		version,
+		urls: () => [`https://dl.google.com/android/cli/${version}/windows_x86_64/android.exe|x64`],
+		releaseNotes: {
+			releaseNotesUrl: 'https://developer.android.com/tools/agents/android-cli/release-notes',
+		},
+	};
+}
