d93e2eec717f59ddd780c24eb838dae81eafb5dd
UnownPlain <git@unownplain.dev>
2026-07-08 15:38:42 -0400

    Proton.ProtonPass: Update URLs (#434)

--- shards/script/Proton.ProtonPass.ts
+++ shards/script/Proton.ProtonPass.ts
@@ -9,9 +9,7 @@ export default defineShard(async () => {
 	const versions = releases.Releases.filter((version) => version.CategoryName === 'Stable');
 
 	const version = versions[0]?.Version;
-	const urls = () => [
-		`https://proton.me/download/pass/windows/ProtonPass_Setup_${version}.exe|x64`,
-	];
+	const urls = () => [`https://proton.me/download/pass/windows/ProtonPass_${version}.msix`];
 
 	return {
 		version,
