0392a486288233de784320559b66c591558386ba
UnownPlain <git@unownplain.dev>
2026-07-21 03:13:39 -0400

    Google.AndroidStudio.*: Switch to JSON source

--- shards/script/Google.AndroidStudio.Beta.ts
+++ shards/script/Google.AndroidStudio.Beta.ts
@@ -1,17 +1,22 @@
 import ky from 'ky';
 
-import { match } from '@/helpers.ts';
 import { defineShard } from '@/schema/script-shard.ts';
 
 export default defineShard(async () => {
-	const response = await ky('https://developer.android.com/studio/preview/').text();
-	const [url, version] = match(
-		response,
-		/beta_win_bundle_download[\s\S]+?href="(https:\/\/edgedl\.me\.gvt1\.com\/android\/studio\/install\/(\d+(?:\.\d+)+)\/android-studio-[^"]*windows\.exe)"/i,
-	);
+	const releases = await ky('https://jb.gg/android-studio-releases-list.json').json<{
+		content: {
+			item: Array<{
+				channel: string;
+				version: string;
+				download: Array<{ link: string }>;
+			}>;
+		};
+	}>();
+	const release = releases.content.item.find(({ channel }) => channel === 'RC');
+	const url = release?.download.find(({ link }) => link.endsWith('-windows.exe'))?.link;
 
 	return {
-		version,
+		version: release?.version,
 		urls: () => [`${url}|x64`],
 		releaseNotes: {
 			releaseNotesUrl: 'https://androidstudio.googleblog.com/',
